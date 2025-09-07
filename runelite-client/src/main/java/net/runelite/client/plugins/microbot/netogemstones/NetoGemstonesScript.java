b6e7aeb70623cf163126bdfb0b871cb4135721b3
Netoxic <ernesto.reza.torres@gmail.com>
2025-09-07 14:52:48 -0600

    Changed Mining logic to find both rocks.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/netogemstones/NetoGemstonesScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/netogemstones/NetoGemstonesScript.java
@@ -6,7 +6,10 @@ import net.runelite.api.Skill;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
+import net.runelite.client.plugins.microbot.util.antiban.Rs2Antiban;
+import net.runelite.client.plugins.microbot.util.antiban.enums.Activity;
 import net.runelite.client.plugins.microbot.util.bank.Rs2Bank;
+import net.runelite.client.plugins.microbot.util.camera.Rs2Camera;
 import net.runelite.client.plugins.microbot.util.gameobject.Rs2GameObject;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
 import net.runelite.client.plugins.microbot.util.player.Rs2Player;
@@ -18,50 +21,33 @@ public class NetoGemstonesScript extends Script {
 
     public static String VERSION = "1.0.0";
     private static final int GEM_ROCK = 11380;
+    private static final int GEM_ROCK_2 = 11381;
     private static final int BANK_DEPOSIT_CHEST = 10530;
 
     private NetoGemstonesState state = NetoGemstonesState.MINING;
 
     public boolean run(NetoGemstonesConfig config) {
+
+        // SETUP
+        Microbot.enableAutoRunOn = false;
+        Rs2Antiban.resetAntibanSettings();
+        Rs2Antiban.antibanSetupTemplates.applyMiningSetup();
+        Rs2Antiban.setActivity(Activity.MINING_GEMSTONES);
+        Rs2Camera.setZoom(200);
+        Rs2Camera.setPitch(512);
+        Rs2Camera.setYaw(1024);
+        sleepGaussian(700, 200);
+
         mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
             try {
                 if (!super.run()) return;
 
                 switch (state) {
                     case MINING:
-                        if (Rs2Inventory.isFull()) {
-                            state = NetoGemstonesState.BANKING;
-                            return;
-                        }
-
-                        GameObject gemRock = Rs2GameObject.getGameObject(GEM_ROCK);
-                        if (gemRock != null) {
-                            if (Rs2GameObject.interact(gemRock, "Mine")) {
-                                Rs2Player.waitForAnimation();
-                            }
-                        }
+                        doMining();
                         break;
                     case BANKING:
-                        if (!Rs2Inventory.isFull()) {
-                            state = NetoGemstonesState.MINING;
-                            return;
-                        }
-
-                        GameObject depositChest = Rs2GameObject.getGameObject(BANK_DEPOSIT_CHEST);
-                        if (depositChest != null) {
-                           if (Rs2GameObject.interact(depositChest, "Deposit")) {
-                               if (sleepUntil(() -> Rs2Widget.hasWidget("Deposit Box"))) {
-                                   if (Rs2Inventory.hasItem("Open gem bag")) {
-                                        Rs2Bank.emptyGemBag();
-                                        sleepGaussian(600, 200);
-                                   }
-//                                   Rs2Widget.clickWidget("Deposit inventory");
-                                   Rs2Bank.depositAllExcept("Open gem bag", "Dragon pickaxe");
-                                   sleepGaussian(900, 300);
-                                   Rs2Bank.closeBank();
-                               }
-                           }
-                        }
+                        doBanking();
                         break;
                 }
 
@@ -70,6 +56,41 @@ public class NetoGemstonesScript extends Script {
             }
         }, 0, 600, TimeUnit.MILLISECONDS);
         return true;
+    }
+
+    public void doMining() {
+        if (Rs2Inventory.isFull()) {
+            state = NetoGemstonesState.BANKING;
+            return;
+        }
+        GameObject gemRock = Rs2GameObject.getGameObject("Gem rocks");
+        if (gemRock != null) {
+            if (Rs2GameObject.interact(gemRock, "Mine")) {
+                Rs2Player.waitForXpDrop(Skill.MINING);
+            }
+        }
+    }
+
+    public void doBanking() {
+        if (!Rs2Inventory.isFull()) {
+            state = NetoGemstonesState.MINING;
+            return;
+        }
+
+        GameObject depositChest = Rs2GameObject.getGameObject(BANK_DEPOSIT_CHEST);
+        if (depositChest != null) {
+            if (Rs2GameObject.interact(depositChest, "Deposit")) {
+                if (sleepUntil(() -> Rs2Widget.hasWidget("Deposit Box"))) {
+                    if (Rs2Inventory.hasItem("Open gem bag")) {
+                        Rs2Bank.emptyGemBag();
+                        sleepGaussian(600, 200);
+                    }
+                    Rs2Widget.clickWidget("Deposit inventory");
+                    sleepGaussian(900, 300);
+                    Rs2Bank.closeBank();
+                }
+            }
+        }
     }
 
     @Override
